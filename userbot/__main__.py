4ca3a71ed36e2d10c3cb2039c3fbdcaaf23f763f
H1M4N5HU0P <82947455+H1M4N5HU0P@users.noreply.github.com>
2021-04-22 02:17:18 +0530

    Add files via upload

---
+++ userbot/__main__.py
@@ -0,0 +1,59 @@
+from userbot import bot
+from sys import argv
+import sys
+from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
+import os
+from telethon import TelegramClient
+from var import Var
+from userbot.Config import Config
+from userbot.utils import load_module
+from userbot import LOAD_PLUG, LOGS, mafiaversion
+from pathlib import Path
+import asyncio
+import telethon.utils
+
+
+async def add_bot(bot_token):
+    await bot.start(bot_token)
+    bot.me = await bot.get_me() 
+    bot.uid = telethon.utils.get_peer_id(bot.me)
+
+
+
+if len(argv) not in (1, 3, 4):
+    bot.disconnect()
+else:
+    bot.tgbot = None
+    if Var.TG_BOT_USER_NAME_BF_HER is not None:
+        print("Initiating Inline Bot")
+        # ForTheGreatrerGood of beautification
+        bot.tgbot = TelegramClient(
+            "TG_BOT_TOKEN",
+            api_id=Var.APP_ID,
+            api_hash=Var.API_HASH
+        ).start(bot_token=Var.TG_BOT_TOKEN_BF_HER)
+        print("Initialisation finished with no errors")
+        print("Starting Userbot")
+        bot.loop.run_until_complete(add_bot(Var.TG_BOT_USER_NAME_BF_HER))
+        print("Startup Completed")
+    else:
+        bot.start()
+
+
+import glob
+path = 'userbot/plugins/*.py'
+files = glob.glob(path)
+for name in files:
+    with open(name) as f:
+        path1 = Path(f.name)
+        shortname = path1.stem
+        load_module(shortname.replace(".py", ""))
+
+import userbot._core
+
+print(f"""MAFIABOT IS ON!!! MAFIABOT VERSION :- {mafiaversion} YOUR 𝕄𝔸𝔽𝕀𝔸𝔹𝕆𝕋 IS READY TO USE! FOR CHECK YOUR BOT WORKING OR NOT PLEASE TYPE (.alive/.ping) ENJOY YOUR BOT! JOIN FOR MORE FUTURE UPDATES @MafiaBot_Support .""")
+
+if len(argv) not in (1, 3, 4):
+    bot.disconnect()
+else:
+    bot.run_until_disconnected()
---
+++ userbot/_core.py
@@ -0,0 +1,144 @@
+import asyncio
+import os
+from datetime import datetime
+from pathlib import Path
+from telethon import events
+from telethon import functions, types
+from telethon.tl.types import InputMessagesFilterDocument
+from mafiabot.utils import *
+from userbot import *
+from userbot import bot as mafiabot
+
+DELETE_TIMEOUT = 5
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Mafia User"
+mafia_logo = "./H1M4N5HU0P/mafiabot_logo.jpg"
+h1m4n5hu0p = mafiabot.uid
+mafia = f"[{DEFAULTUSER}](tg://user?id={h1m4n5hu0p})"
+
+@mafiabot.on(admin_cmd(pattern=r"send (?P<shortname>\w+)", outgoing=True))
+@mafiabot.on(sudo_cmd(pattern=r"send (?P<shortname>\w+)", allow_sudo=True))
+async def send(event):
+    if event.fwd_from:
+        return
+    message_id = event.message.id
+    thumb = mafia_logo
+    input_str = event.pattern_match.group(1)
+    omk = f"**⍟ Plugin name ≈** `{input_str}`\n**⍟ Uploaded by ≈** {mafia}\n\n⚡ **[LEGENDARY AF MAFIABOT](t.me/MafiaBot_Support)** ⚡"
+    the_plugin_file = "./userbot/plugins/{}.py".format(input_str)
+    if os.path.exists(the_plugin_file):
+        lauda = await event.client.send_file(
+            event.chat_id,
+            the_plugin_file,
+            thumb=thumb,
+            caption=omk,
+            force_document=True,
+            allow_cache=False,
+            reply_to=message_id,
+        )
+        await event.delete()
+    else:
+        await edit_or_reply(event, "File not found..... Kek")
+
+@mafiabot.on(admin_cmd(pattern="install$", outgoing=True))
+@mafiabot.on(sudo_cmd(pattern="install$", allow_sudo=True))
+async def install(event):
+    if event.fwd_from:
+        return
+    a = "__Installing.__"
+    b = 1
+    await event.edit(a)
+    if event.fwd_from:
+        return
+    if event.reply_to_msg_id:
+        try:
+            downloaded_file_name = await event.client.download_media(  # pylint:disable=E0602
+                await event.get_reply_message(),
+                "./userbot/plugins/"  # pylint:disable=E0602
+            )
+            if "(" not in downloaded_file_name:
+                path1 = Path(downloaded_file_name)
+                shortname = path1.stem
+                load_module(shortname.replace(".py", ""))
+                if shortname in CMD_LIST:
+                    string = "**Commands found in** `{}` (sudo included)\n".format((os.path.basename(downloaded_file_name)))
+                    for i in CMD_LIST[shortname]:
+                        string += "  •  `" + i 
+                        string += "`\n"
+                        if b == 1:
+                            a = "__Installing..__"
+                            b = 2
+                        else:
+                            a = "__Installing...__"
+                            b = 1
+                        await event.edit(a)
+                    return await event.edit(f"✅ **Installed module** :- `{shortname}` \n✨ BY :- {mafia}\n\n{string}\n\n        ⚡ **[LEGENDARY AF MAFIABOT](t.me/MafiaBot_Support)** ⚡", link_preview=False)
+                return await event.edit(f"Installed module `{os.path.basename(downloaded_file_name)}`")
+            else:
+                os.remove(downloaded_file_name)
+                return await event.edit(f"**Failed to Install** \n`Error`\nModule already installed or unknown format")
+        except Exception as e: 
+            await event.edit(f"**Failed to Install** \n`Error`\n{str(e)}")
+            return os.remove(downloaded_file_name)
+    
+@mafiabot.on(admin_cmd(pattern=r"uninstall (?P<shortname>\w+)", outgoing=True))
+@mafiabot.on(sudo_cmd(pattern=r"uninstall (?P<shortname>\w+)", allow_sudo=True))
+async def uninstall(h1m4n5hu0p):
+    if h1m4n5hu0p.fwd_from:
+        return
+    shortname = h1m4n5hu0p.pattern_match["shortname"]
+    dir_path =f"./userbot/plugins/{shortname}.py"
+    try:
+        remove_plugin(shortname)
+        os.remove(dir_path)
+        await h1m4n5hu0p.edit(f"Uninstalled `{shortname}` successfully")
+    except OSError as e:
+        await h1m4n5hu0p.edit("Error: %s : %s" % (dir_path, e.strerror))
+
+@mafiabot.on(admin_cmd(pattern=r"unload (?P<shortname>\w+)$"))
+@mafiabot.on(sudo_cmd(pattern=r"upload (?P<shortname>\w+)$", allow_sudo=True))
+async def unload(event):
+    if event.fwd_from:
+        return
+    shortname = event.pattern_match["shortname"]
+    try:
+        remove_plugin(shortname)
+        await event.edit(f"Successfully unloaded `{shortname}`")
+    except Exception as e:
+        await event.edit(
+            "Successfully unloaded {shortname}\n{}".format(
+                shortname, str(e)
+            )
+        )
+
+
+@mafiabot.on(admin_cmd(pattern=r"load (?P<shortname>\w+)$"))
+@mafiabot.on(sudo_cmd(pattern=r"load (?P<shortname>\w+)$", allow_sudo=True))
+async def load(event):
+    if event.fwd_from:
+        return
+    shortname = event.pattern_match["shortname"]
+    try:
+        try:
+            remove_plugin(shortname)
+        except BaseException:
+            pass
+        load_module(shortname)
+        await event.edit(f"Successfully loaded `{shortname}`")
+    except Exception as e:
+        await event.edit(
+            f"Sorry, could not load {shortname} because of the following error.\n{str(e)}"
+        )
+
+CmdHelp("core").add_command(
+  "install", "<reply to a .py file>", "Installs the replied python file if suitable to userbot codes. (TEMPORARILY DISABLED AS HACKERS MAKE YOU INSTALL SOME PLUGINS AND GET YOUR DATA)"
+).add_command(
+  "uninstall", "<plugin name>", "Uninstalls the given plugin from userbot. To get that again do .restart", "uninstall alive"
+).add_command(
+  "load", "<plugin name>", "Loades the unloaded plugin to your userbot", "load alive"
+).add_command(
+  "unload", "<plugin name>", "Unloads the plugin from your userbot", "unload alive"
+).add_command(
+  "send", "<file name>", "Sends the given file from your userbot server, if any.", "send alive"
+).add_command(
+  "cmds", None, "Gives out the list of modules in mafiabot."
+).add()
