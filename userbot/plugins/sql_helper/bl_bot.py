bd42f63f11ea3d64279ae6b1020e23037cedcdd4
H1M4N5HU0P <82947455+H1M4N5HU0P@users.noreply.github.com>
2021-04-22 02:18:58 +0530

    Add files via upload

---
+++ userbot/plugins/sql_helper/bl_bot.py
@@ -0,0 +1,42 @@
+from sqlalchemy import Column, String
+
+from hellbot.plugins.sql_helper import BASE, SESSION
+
+
+class Blockedid(BASE):
+    __tablename__ = "blockedid"
+    chat_id = Column(String(14), primary_key=True)
+
+    def __init__(self, chat_id):
+        self.chat_id = chat_id
+
+
+Blockedid.__table__.create(checkfirst=True)
+
+
+def add_in_db(chat_id: int):
+    lodu = Blockedid(str(chat_id))
+    SESSION.add(lodu)
+    SESSION.commit()
+
+
+def get_all_nibba():
+    eberyone = SESSION.query(Blockedid).all()
+    SESSION.close()
+    return eberyone
+
+
+def is_id_added(chat_id):
+    try:
+        return SESSION.query(Blockedid).filter(Blockedid.chat_id == str(chat_id)).one()
+    except:
+        return None
+    finally:
+        SESSION.close()
+
+
+def removeid(chat_id):
+    safai = SESSION.query(Blockedid).get(str(chat_id))
+    if safai:
+        SESSION.delete(safai)
+        SESSION.commit()
