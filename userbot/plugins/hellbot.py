8a6a4350e9a38a5fa9067df3fc5f0ba841a2c592
H1M4N5HU0P <82947455+H1M4N5HU0P@users.noreply.github.com>
2021-04-22 20:46:11 +0530

    Add files via upload

--- userbot/plugins/hellbot.py
+++ userbot/plugins/hellbot.py
@@ -15,7 +15,7 @@ from telethon import events
 @borg.on(admin_cmd(pattern="hellbot ?(.*)"))
 async def _(event):
      if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
-        await event.edit("Click [here](https://github.com/HellBoy-OP/HellBot) to open this 🔥**Lit AF!!**🔥 **Hêllẞø†** Repo.. Join channel :- @HellBot_Official Repo Uploaded By [𝕄𝔸𝔽𝕀𝔸 𝕌𝕊𝔼ℝ𝔹𝕆𝕋](https://t.me/MafiaBot_Support")
+        await event.edit("Click [here](https://github.com/HellBoy-OP/HellBot) to open this 🔥**Lit AF!!**🔥 **Hêllẞø†** Repo.. Join channel :- @HellBot_Official Repo Uploaded By @MafiaBot_Support")
     
         
    
