04505ab22db78b388f011c02089b1a4756dfed4b
H1M4N5HU0P <82947455+H1M4N5HU0P@users.noreply.github.com>
2021-05-03 19:27:27 +0530

    Add files via upload

--- userbot/plugins/__init__.py
+++ userbot/plugins/__init__.py
@@ -3,5 +3,6 @@ from userbot.utils import *
 from userbot.Config import Config
 from userbot.helpers.functions import *
 from userbot.cmdhelp import CmdHelp
-from userbot.format import *
+from userbot.helpers.events import *
+from userbot.helpers.format import *
 from userbot.helpers.utils import *
