e36246728e64c0ec0370ce67c74f9e0b7fe54926
H1M4N5HU0P <82947455+H1M4N5HU0P@users.noreply.github.com>
2021-05-04 00:31:46 +0530

    Update globalban.py

--- userbot/plugins/globalban.py
+++ userbot/plugins/globalban.py
@@ -311,33 +311,34 @@ async def mafiagkick(event):
         if reply:
             await reply.forward_to(BOTLOG_CHATID)
 
-
+ 
 @borg.on(ChatAction)
-async def handler(h1m4n5hu0p): 
-   if h1m4n5hu0p.user_joined or h1m4n5hu0p.user_added:      
+async def handler(him): 
+   if him.user_joined or him.user_added:      
        try:       	
          from userbot.plugins.sql_helper.gmute_sql import is_gmuted
-         guser = await h1m4n5hu0p.get_user()      
+         guser = await him.get_user_from_event()      
          gmuted = is_gmuted(guser.id)             
        except:      
           return
        if gmuted:
         for i in gmuted:
             if i.sender == str(guser.id):                                                                         
-                chat = await h1m4n5hu0p.get_chat()
+                chat = await him.get_chat()
                 admin = chat.admin_rights
                 creator = chat.creator   
                 if admin or creator:
                  try:
-                    await client.edit_permissions(h1m4n5hu0p.chat_id, guser.id, view_messages=False)                              
-                    await h1m4n5hu0p.reply(
+                    await client.edit_permissions(him.chat_id, guser.id, view_messages=False)                              
+                    await him.reply(
                      f"⚠️⚠️**Warning**⚠️⚠️\n\n`Gbanned User Joined the chat!!`\n"                      
                      f"**⚜️ Victim Id ⚜️**:\n[{guser.id}](tg://user?id={guser.id})\n"                   
                      f"**🔥 Action 🔥**  :\n`Banned this piece of shit....` **AGAIN!**")                                                
                  except:       
-                    h1m4n5hu0p.reply("`Sheit!! No permission to ban users.\n@admins ban this retard.\nGlobally Banned User And A Potential Spammer`\n**Make your group a safe place by cleaning this shit**")                   
+                    him.reply("`Shit!! No permission to ban users.\n@admins ban this retard.\nGlobally Banned User And A Potential Spammer`\n**Make your group a safe place by cleaning this shit**")                   
                     return
-     
+                  
+           
                   
 CmdHelp("globalban").add_command(
   'tgban', '<reply> / <userid> / <username>', 'Gbans the targeted user and adds to gban watch list'
