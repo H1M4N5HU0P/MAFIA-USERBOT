9df1f3234fd04472c9dd52c7140ba8c6a1a7d605
H1M4N5HU0P <82947455+H1M4N5HU0P@users.noreply.github.com>
2021-05-03 19:19:59 +0530

    Add files via upload

--- userbot/plugins/broadcast.py
+++ userbot/plugins/broadcast.py
@@ -6,8 +6,9 @@ from asyncio import sleep
 
 from telethon.tl.functions.messages import ImportChatInviteRequest as Get
 from mafiabot.utils import admin_cmd, edit_or_reply, sudo_cmd
-from . import BOTLOG, BOTLOG_CHATID, parse_pre
-from .sql_helper import broadcast_sql as sql
+from userbot import BOTLOG, BOTLOG_CHATID
+from userbot.helpers import parse_pre
+from userbot.sql_helper import broadcast_sql as sql
 from userbot.cmdhelp import CmdHelp
 
 @bot.on(admin_cmd(pattern="sendto(?: |$)(.*)", command="sendto"))
