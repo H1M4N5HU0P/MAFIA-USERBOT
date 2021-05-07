dce538834323efbb6a84c5132ed527ac1a7c2f86
H1M4N5HU0P <82947455+H1M4N5HU0P@users.noreply.github.com>
2021-05-07 17:42:43 +0530

    Add files via upload

--- userbot/plugins/translate.py
+++ userbot/plugins/translate.py
@@ -51,8 +51,7 @@ async def _(event):
 async def _(mafia):
     if mafia.fwd_from:
         return
-    await edit_or_reply(mafia, "**All The Language Codes Can Be Found** \n ⚡ [Here](https://telegra.ph/%F0%9D%95%B1-%F0%9D%95%BE-%F0%9D% NB95%B1--H%C3%A8ll%E1%BA%9E%C3%B8y-%F0%90%8C%B7%F0%90%8C%B4%E0%A0%8B%E0%A0%8B%F0%90%8C%B1%F0%90%8D%88%F0%90%8C%B8-%F0%90%8C%BE%F0%90%8C%B0%F0%90%8D%80%F0%90%8C%BE-B%E3%83%A0JRANGD%E3%83%A0L-12-04) ⚡")
-
+    await edit_or_reply(mafia, "**All The Language Codes Can Be Found** \n ⚡ [Here](https://telegra.ph/Himanshu-05-07) ⚡🔥")
 
 
 CmdHelp("translate").add_command(
