3dd38a322c1a54e8819a61cfe74ff3305d8d30a6
H1M4N5HU0P <82947455+H1M4N5HU0P@users.noreply.github.com>
2021-05-11 10:20:44 +0530

    Add files via upload

---
+++ userbot/plugins/xalive.py
@@ -0,0 +1,116 @@
+
+# Thanks to Sipak bro and Aryan.. 
+# animation Idea by @NOOB_GUY_OP (Sipakisking) && @Hell boy_pikachu
+# Made by @ROMANTIC_KILLER...and thanks to @Crackexy for the logos...
+# Kang with credits else gay...
+# Porting in Mafia Userbot by @H1M4N5HU0P
+
+import asyncio
+import random
+from telethon import version, __version__
+from telethon import events
+from userbot import ALIVE_NAME, mafiaversion
+from mafiabot.utils import admin_cmd, sudo_cmd
+from telethon.tl.types import ChannelParticipantsAdmins
+
+
+# 🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "𝕄𝔸𝔽𝕀𝔸𝔹𝕆𝕋"
+
+# Thanks to Sipak bro and Raganork.. 
+# animation Idea by @NOOB_GUY_OP (Sipakisking)
+# Made by @ROMANTIC_KILLER...and thanks to @Crackexy for the logos...
+# Kang with credits else gay...
+# alive.py for ÂÝŮ$HópBØȚ
+
+ludosudo = Config.SUDO_USERS
+
+if ludosudo:
+    sudou = "True"
+else:
+    sudou = "False"
+
+mafia = bot.uid
+
+edit_time = 16
+""" =======================CONSTANTS====================== """
+file1 = "https://telegra.ph/file/2408a2877646132ac52fd.mp4"
+file2 = "https://telegra.ph/file/e97d640332ce5eadb3f89.mp4"
+file3 = "https://telegra.ph/file/0b2862d312a2aeb804b36.mp4"
+file4 = "https://telegra.ph/file/866c79e351350a08f2b06.mp4"
+""" =======================CONSTANTS====================== """
+pm_caption = "  __**🔥🔥𝕄𝔸𝔽𝕀𝔸𝔹𝕆𝕋 𝕀𝕊 𝔸𝕃𝕀𝕍𝔼🔥🔥**__\n\n"
+
+pm_caption += (
+    f"                 👑𝕄𝔸𝕊𝕋𝔼ℝ👑\n**  『😈[{DEFAULTUSER}](tg://user?id={mafia})😈』**\n\n"
+)
+
+pm_caption += "🛡️TELETHON🛡️ : `{version.__version__}` \n\n"
+
+pm_caption += f"😈𝕄𝔸𝔽𝕀𝔸𝔹𝕆𝕋😈 : `{mafiaversion}`\n\n"
+
+pm_caption += f"😱SUDO😱            : `{sudou}`\n\n"
+
+pm_caption += "😇CHANNEL😇️   : [ᴊᴏɪɴ](https://t.me/MafiaBot_Support)\n\n"
+
+pm_caption += "😎CREATOR😎    : [Himanshu](https://t.me/H1M4N5HU0P)\n\n"
+
+pm_caption += "🤩SUPPORTER🤩    :[HellBoy](https://t.me/kraken_the_badass)\n\n"
+
+pm_caption += "      [🔥REPO🔥](https://github.com/H1M4N5HU0P/MAFIA-BOT) 🔹 [📜License📜](https://github.com/H1M4N5HU0P/MAFIA-BOT/blob/main/LICENSE)"
+
+# @command(outgoing=True, pattern="^.alive$")
+@bot.on(admin_cmd(outgoing=True, pattern="xalive$"))
+@bot.on(sudo_cmd(pattern="xalive$", allow_sudo=True))
+async def amireallyalive(alive):
+    await alive.get_chat()   
+    
+    on = await borg.send_file(alive.chat_id, file=file1,caption=pm_caption)
+
+    await asyncio.sleep(edit_time)
+    ok = await borg.edit_message(alive.chat_id, on, file=file2) 
+
+    await asyncio.sleep(edit_time)
+    ok2 = await borg.edit_message(alive.chat_id, ok, file=file3)
+
+    await asyncio.sleep(edit_time)
+    ok3 = await borg.edit_message(alive.chat_id, ok2, file=file1)
+    
+    await asyncio.sleep(edit_time)
+    ok4 = await borg.edit_message(alive.chat_id, ok3, file=file3)
+    
+    await asyncio.sleep(edit_time)
+    ok5 = await borg.edit_message(alive.chat_id, ok4, file=file2)
+    
+    await asyncio.sleep(edit_time)
+    ok6 = await borg.edit_message(alive.chat_id, ok5, file=file4)
+    
+    await asyncio.sleep(edit_time)
+    ok7 = await borg.edit_message(alive.chat_id, ok6, file=file1)
+    
+    await asyncio.sleep(edit_time)
+    ok8 = await borg.edit_message(alive.chat_id, ok7, file=file2) 
+
+    await asyncio.sleep(edit_time)
+    ok9 = await borg.edit_message(alive.chat_id, ok8, file=file3)
+
+    await asyncio.sleep(edit_time)
+    ok10 = await borg.edit_message(alive.chat_id, ok9, file=file1)
+    
+    await asyncio.sleep(edit_time)
+    ok11 = await borg.edit_message(alive.chat_id, ok10, file=file3)
+    
+    await asyncio.sleep(edit_time)
+    ok12 = await borg.edit_message(alive.chat_id, ok11, file=file2)
+    
+    await asyncio.sleep(edit_time)
+    ok13 = await borg.edit_message(alive.chat_id, ok12, file=file4)
+    
+    await asyncio.sleep(edit_time)
+    ok14 = await borg.edit_message(alive.chat_id, on, file=file1)
+
+    """ For .alive command, check if the bot is running.  """
+    await borg.send_file(alive.chat_id, caption=pm_caption)
+    await alive.delete()
+    
+    
