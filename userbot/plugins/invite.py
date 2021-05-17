da5e2ae82caf79d6f55cd53c7a291229b12e968d
H1M4N5HU0P <82947455+H1M4N5HU0P@users.noreply.github.com>
2021-05-17 18:16:34 +0530

    Update invite.py

--- userbot/plugins/invite.py
+++ userbot/plugins/invite.py
@@ -103,8 +103,8 @@ async def get_users(event):
     )
 
 
-@bot.on(admin_cmd(pattern="add ?(.*)"))
-@bot.on(sudo_cmd(pattern="add ?(.*)", allow_sudo=True))
+@bot.on(admin_cmd(pattern="invite ?(.*)"))
+@bot.on(sudo_cmd(pattern="invite ?(.*)", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
