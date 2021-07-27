5a20ff0cfeb9d8c5eac2afe6688a7be89bb416f4
H1M4N5HU0P <82947455+H1M4N5HU0P@users.noreply.github.com>
2021-07-27 14:42:17 +0530

    Add files via upload

--- userbot/plugins/mafiabot.py
+++ userbot/plugins/mafiabot.py
@@ -2,7 +2,7 @@ import time
 
 from userbot import ALIVE_NAME, StartTime, mafiaversion
 from mafiabot.utils import admin_cmd, edit_or_reply, sudo_cmd
-
+from telethon import events, version
 
 async def reply_id(event):
     reply_to_id = None
