a872794fa4ced24c2cf079733f25d537c21e8f93
D3KRISH <83129000+D3KRISH@users.noreply.github.com>
2021-08-30 22:19:06 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -57,9 +57,9 @@ print(f"""MAFIABOT IS ON!!! MAFIABOT VERSION :- {mafiaversion} YOUR 𝕄𝔸𝔽
 
 async def mafia_is_on():
     try:
-        if Config.PRIVATE_GROUP_ID != 0:
+        if Config.PM_LOGGR_BOT_API_ID != 0:
             await bot.send_file(
-                Config.PRIVATE_GROUP_ID,
+                Config.PM_LOGGR_BOT_API_ID,
                 MAFIA_PIC,
                 caption=f"༆ʟɛɢɛռɖaʀʏ ᴀғ ᴍᴀғɪᴀʙᴏᴛ༆\n\n**𝚅𝙴𝚁𝚂𝙸𝙾𝙽 ➪ {mafiaversion}**\n\n𝐓𝐲𝐩𝐞 `{hl}ping` or `{hl}alive` 𝐭𝐨 𝐜𝐡𝐞𝐜𝐤! \n\n𝙹𝙾𝙸𝙽 [𝙼𝙰𝙵𝙸𝙰𝙱𝙾𝚃 𝙲𝙷𝙰𝚃](t.me/MafiaBot_Support) 𝚃𝙾 𝚀𝚄𝙴𝚁𝚈 & 𝙹𝙾𝙸𝙽 [𝙼𝙰𝙵𝙸𝙰 𝚄𝙿𝙳𝙰𝚃𝙴𝚂](t.me/MAFIABOT_CHIT_CHAT) 𝚃𝙾 𝙺𝙽𝙾𝚆 𝚁𝙴𝙶𝚁𝙰𝙳𝙸𝙽𝙶 𝚄𝙿𝙳𝙰𝚃𝙴 𝙰𝙽𝙳 𝙽𝙴𝚆𝚂 𝙰𝙱𝙾𝚄𝚃 **𝙼𝙰𝙵𝙸𝙰𝙱𝙾𝚃**",
             )
