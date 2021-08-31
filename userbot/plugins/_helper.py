98b9519ffafbc6d718dedfa1c999f067e7a76346
D3KRISH <83129000+D3KRISH@users.noreply.github.com>
2021-08-31 15:18:31 +0530

    Update _helper.py

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -3,45 +3,10 @@ import asyncio
 
 import requests
 from telethon import functions
-from telethon.errors import ChatSendInlineForbiddenError as noin
-from telethon.errors.rpcerrorlist import BotMethodInvalidError as dedbot, BotInlineDisabledError as noinline, YouBlockedUserError
 
 from userbot import ALIVE_NAME, CMD_LIST, SUDO_LIST
 from mafiabot.utils import admin_cmd, edit_or_reply, sudo_cmd
 
-@bot.on(admin_cmd(pattern="repo ?(.*)", outgoing=True))
-@bot.on(sudo_cmd(pattern="repo ?(.*)", allow_sudo=True))
-async def _(event):
-    if event.fwd_from:
-        return
-    tgbotusername = Config.TG_BOT_USER_NAME_BF_HER
-    chat = "@Botfather"
-    if tgbotusername is not None:
-        try:
-            results = await event.client.inline_query(tgbotusername, "hellbot_help")
-            await results[0].click(
-                event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
-            )
-            await event.delete()
-        except noinline:
-            mafia = await edit_or_reply(event, "**Inline Mode is disabled.** \n__Turning it on, please wait for a minute...__")
-            async with bot.conversation(chat) as conv:
-                try:
-                    first = await conv.send_message("/setinline")
-                    second = await conv.get_response()
-                    third = await conv.send_message(tgbotusername)
-                    fourth = await conv.get_response()
-                    fifth = await conv.send_message(perf)
-                    sixth = await conv.get_response()
-                    await bot.send_read_acknowledge(conv.chat_id)
-                except YouBlockedUserError:
-                    return await mafia.edit("Unblock @Botfather first.")
-                await mafia.edit(f"**Turned On Inline Mode Successfully.** \n\nDo `.help or .repo` again to get the menu.")
-            await bot.delete_messages(
-                conv.chat_id, [first.id, second.id, third.id, fourth.id, fifth.id, sixth.id]
-            )
-    else:
-        await edit_or_reply(event, "**⚠️ ERROR !!** \nPlease Re-Check BOT_TOKEN & BOT_USERNAME on Heroku.")
 
 @bot.on(admin_cmd(pattern="help ?(.*)", outgoing=True))
 async def yardim(event):
