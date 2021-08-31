fa691f7ec5b2b3a78b28cf06523c2e1327b9ebca
D3KRISH <83129000+D3KRISH@users.noreply.github.com>
2021-08-31 09:01:01 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -53,6 +53,18 @@ for name in files:
 import userbot._core
 
 print(f"""MAFIABOT IS ON!!! MAFIABOT VERSION :- {mafiaversion} YOUR 𝕄𝔸𝔽𝕀𝔸𝔹𝕆𝕋 IS READY TO USE! FOR CHECK YOUR BOT WORKING OR NOT PLEASE TYPE (.alive/.ping) ENJOY YOUR BOT! JOIN FOR MORE FUTURE UPDATES @MafiaBot_Support .""")
+async def mafia_is_on():
+    try:
+        if Config.PM_LOGGR_BOT_API_ID != 0:
+            await bot.send_file(
+                Config.PM_LOGGR_BOT_API_ID,
+                MAFIA_PIC,
+                caption=f"༆ʟɛɢɛռɖaʀʏ ᴀғ ᴍᴀғɪᴀʙᴏᴛ༆\n\n**𝚅𝙴𝚁𝚂𝙸𝙾𝙽 ➪ {mafiaversion}**\n\n𝐓𝐲𝐩𝐞 .ping or .alive 𝐭𝐨 𝐜𝐡𝐞𝐜𝐤! \n\n𝙹𝙾𝙸𝙽 [𝙼𝙰𝙵𝙸𝙰𝙱𝙾𝚃 𝙲𝙷𝙰𝚃](t.me/MAFIABOT_CHIT_CHAT) 𝚃𝙾 𝚀𝚄𝙴𝚁𝚈 & 𝙹𝙾𝙸𝙽 [𝙼𝙰𝙵𝙸𝙰 𝚄𝙿𝙳𝙰𝚃𝙴𝚂](t.me/MafiaBot_Support) 𝚃𝙾 𝙺𝙽𝙾𝚆 𝚁𝙴𝙶𝚁𝙰𝙳𝙸𝙽𝙶 𝚄𝙿𝙳𝙰𝚃𝙴 𝙰𝙽𝙳 𝙽𝙴𝚆𝚂 𝙰𝙱𝙾𝚄𝚃 𝙼𝙰𝙵𝙸𝙰𝙱𝙾𝚃",
+            )
+    except Exception as e:
+        LOGS.info(str(e))
+
+bot.loop.create_task(mafia_is_on())
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
