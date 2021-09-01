ba7cb5722ea646c87533716be028dd15360974a6
H1M4N5HU0P <82947455+H1M4N5HU0P@users.noreply.github.com>
2021-09-01 18:33:37 +0530

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#   Copyright (C) 2021 Mafiabot
+#
+#   Poject VΛDΣЯ is free software: 
+#   you can redistribute it and/or modify it under the terms of the GNU General Public License 
+#   as published by the Free Software Foundation, either version 3 of the License, 
+#   or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful, 
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+#   See the GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License along with this program. 
+#   If not, see <https://www.gnu.org/licenses/>.
+#
+
+
+echo """
+
+             ╔═╦═╦══╦══╦══╦══╗╔╦╦══╦═╦═╦══╦═╦══╗
+             ║║║║║╔╗║═╦╩║║╣╔╗║║║║══╣╦╣╬║╔╗║║╠╗╔╝
+             ║║║║║╠╣║╔╝╔║║╣╠╣║║║╠══║╩╣╗╣╔╗║║║║║
+             ╚╩═╩╩╝╚╩╝─╚══╩╝╚╝╚═╩══╩═╩╩╩══╩═╝╚╝
+
+                       𝓛𝓔𝓖𝓔𝓝𝓓𝓐𝓡𝓨 𝓐𝓕 𝓜𝓐𝓕𝓘𝓐𝓑𝓞𝓣 !!
+                                          @MAFIA_USERBOT
+"""
+
+python3 -m userbot
