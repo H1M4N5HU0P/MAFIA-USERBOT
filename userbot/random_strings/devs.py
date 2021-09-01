94d23287605bbd76a33d84b9700370731e863a99
D3KRISH <83129000+D3KRISH@users.noreply.github.com>
2021-09-01 06:08:31 +0530

    Create devs.py

---
+++ userbot/random_strings/devs.py
@@ -0,0 +1,14 @@
+HARMFUL = [
+    "SESSION",
+    "HEROKU_API_KEY",
+    "base64",
+    "bash",
+    "get_me()",
+    "phone",
+    "os.system",
+    "sys.stdout",
+    "sys.stderr",
+    "subprocess",
+    "STRING_SESSION",
+    "session.save()",
+]
