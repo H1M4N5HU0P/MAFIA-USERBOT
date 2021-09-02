596d034cfc1be83efc7f7af83e00ce040e83a4f7
D3KRISH <83129000+D3KRISH@users.noreply.github.com>
2021-09-02 15:36:12 +0530

    Create funcs.py

---
+++ mafiabot/utils/funcs.py
@@ -0,0 +1,46 @@
+import asyncio
+import datetime
+import importlib
+import inspect
+import logging
+import math
+import os
+import re
+import sys
+import time
+import traceback
+from pathlib import Path
+from time import gmtime, strftime
+
+from telethon import events
+from telethon.tl.functions.channels import GetParticipantRequest
+from telethon.tl.types import ChannelParticipantAdmin, ChannelParticipantCreator
+
+from mafiabot import *
+from .helpers import *
+from .config import Config
+
+
+# just a small shit for big works
+class Loader:
+    def __init__(self, func=None, **args):
+        self.Var = Var
+        bot.add_event_handler(func, events.NewMessage(**args))
+
+
+# Check if Admin
+async def is_admin(client, chat_id, user_id):
+    if not str(chat_id).startswith("-100"):
+        return False
+    try:
+        mafiaboy = await client(GetParticipantRequest(channel=chat_id, user_id=user_id))
+        chat_participant = d3boy.participant
+        if isinstance(
+            chat_participant, (ChannelParticipantCreator, ChannelParticipantAdmin)
+        ):
+            return True
+    except Exception:
+        return False
+    else:
+        return False
+
