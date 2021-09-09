c464b182f9457af4250ecf55605383a0e889bda3
H1M4N5HU0P <82947455+H1M4N5HU0P@users.noreply.github.com>
2021-09-09 22:20:43 +0530

    Something Osm Here
    OP

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -28,7 +28,7 @@ from userbot.cmdhelp import *
 from mafiabot.utils import *
 from userbot.Config import Config
 
-mafia_help_pic = Config.mafia_help_pic
+mafia_help_pic = Config.ALIVE_PIC
 mafia_row = Config.BUTTONS_IN_HELP
 mafia_emoji = Config.EMOJI_IN_HELP
 # thats how a lazy guy imports
