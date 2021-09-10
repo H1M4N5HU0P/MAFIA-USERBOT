03de6a7be1ad7333ff15972b383a20fd7c38ef11
H1M4N5HU0P <82947455+H1M4N5HU0P@users.noreply.github.com>
2021-09-10 18:48:10 +0530

    Add files via upload

---
+++ userbot/helpers/extras.py
@@ -0,0 +1,101 @@
+import asyncio
+import datetime
+import importlib
+import inspect
+import logging
+import math
+import os
+import re
+import sys
+import time
+import traceback
+from pathlib import Path
+from time import gmtime, strftime
+
+from telethon import events
+from telethon.tl.functions.channels import GetParticipantRequest
+from telethon.tl.types import ChannelParticipantAdmin, ChannelParticipantCreator
+
+from userbot import *
+from userbot.helpers.pasters import pasty
+from userbot.Config import Config
+
+
+# either edit or reply that msg
+async def edit_or_reply(
+    event,
+    text,
+    parse_mode=None,
+    link_preview=None,
+    file_name=None,
+    aslink=False,
+    deflink=False,
+    noformat=False,
+    linktext=None,
+    caption=None,
+):
+    link_preview = link_preview or False
+    reply_to = await event.get_reply_message()
+    if len(text) < 4096 and not deflink:
+        parse_mode = parse_mode or "md"
+        if event.sender_id in Config.SUDO_USERS:
+            if reply_to:
+                return await reply_to.reply(
+                    text, parse_mode=parse_mode, link_preview=link_preview
+                )
+            return await event.reply(
+                text, parse_mode=parse_mode, link_preview=link_preview
+            )
+        await event.edit(text, parse_mode=parse_mode, link_preview=link_preview)
+        return event
+    if not noformat:
+        asciich = ["**", "`", "__"]
+        for i in asciich:
+            text = re.sub(rf"\{i}", "", text)
+    if aslink or deflink:
+        linktext = linktext or "Message was to big so pasted to bin"
+        response = await pasty(text)
+        text = linktext + f"[BIN]({response['url']}) •• [RAW]({response['raw']})"
+        if event.sender_id in Config.SUDO_USERS:
+            if reply_to:
+                return await reply_to.reply(text, link_preview=link_preview)
+            return await event.reply(text, link_preview=link_preview)
+        await event.edit(text, link_preview=link_preview)
+        return event
+    file_name = file_name or "output.txt"
+    caption = caption or None
+    with open(file_name, "w+") as output:
+        output.write(text)
+    if reply_to:
+        await reply_to.reply(caption, file=file_name)
+        await event.delete()
+        return os.remove(file_name)
+    if event.sender_id in Config.SUDO_USERS:
+        await event.reply(caption, file=file_name)
+        await event.delete()
+        return os.remove(file_name)
+    await event.client.send_file(event.chat_id, file_name, caption=caption)
+    await event.delete()
+    os.remove(file_name)
+
+
+# delete timeout
+async def delete_mafia(event, text, time=None, parse_mode=None, link_preview=None):
+    parse_mode = parse_mode or "md"
+    link_preview = link_preview or False
+    time = time or 10
+    if event.sender_id in Config.SUDO_USERS:
+        reply_to = await event.get_reply_message()
+        mafiaevent = (
+            await reply_to.reply(text, link_preview=link_preview, parse_mode=parse_mode)
+            if reply_to
+            else await event.reply(
+                text, link_preview=link_preview, parse_mode=parse_mode
+            )
+        )
+    else:
+        mafiaevent = await event.edit(
+            text, link_preview=link_preview, parse_mode=parse_mode
+        )
+    await asyncio.sleep(time)
+    return await mafiaevent.delete()
---
+++ userbot/helpers/pasters.py
@@ -0,0 +1,52 @@
+import json
+import requests
+
+from html_telegraph_poster import TelegraphPoster
+
+from userbot import *
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36",
+    "content-type": "application/json",
+}
+
+async def pasty(message, extension=None):
+    siteurl = "https://pasty.lus.pm/api/v1/pastes"
+    data = {"content": message}
+    try:
+        response = requests.post(url=siteurl, data=json.dumps(data), headers=headers)
+    except Exception as e:
+        return {"error": str(e)}
+    if response.ok:
+        response = response.json()
+        purl = (
+            f"https://pasty.lus.pm/{response['id']}.{extension}"
+            if extension
+            else f"https://pasty.lus.pm/{response['id']}.txt"
+        )
+        try:
+            await bot.send_message(
+                Config.LOGGER_ID,
+                f"#PASTE \n\n**Open Paste From** [here]({purl}). \n**Delete that paste by using this token** `{response['deletionToken']}`",
+            )
+        except Exception as e:
+            LOGS.info(str(e))
+        return {
+            "url": purl,
+            "raw": f"https://pasty.lus.pm/{response['id']}/raw",
+            "bin": "Pasty",
+        }
+    return {"error": "Unable to reach pasty.lus.pm"}
+
+
+async def telegraph_paste(page_title, temxt):
+    cl1ent = TelegraphPoster(use_api=True)
+    auth = "[ LEGENDARY AF HELLBOT ]"
+    cl1ent.create_api_token(auth)
+    post_page = cl1ent.post(
+        title=page_title,
+        author=auth,
+        author_url="https://t.me/MafiaBot_Support",
+        text=temxt,
+    )
+    return post_page["url"]
