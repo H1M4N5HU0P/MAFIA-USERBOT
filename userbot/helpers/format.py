6ce47c0492a130b041d55468d2108d84298ba74c
H1M4N5HU0P <82947455+H1M4N5HU0P@users.noreply.github.com>
2021-09-10 16:46:02 +0530

    Add files via upload

--- userbot/helpers/format.py
+++ userbot/helpers/format.py
@@ -3,21 +3,14 @@
 
 import datetime
 import re
-import json
 
 import requests
 from telethon.tl.tlobject import TLObject
 from telethon.tl.types import MessageEntityPre
 from telethon.utils import add_surrogate
-from html_telegraph_poster import TelegraphPoster
+
 from userbot.helpers.functions import utc_to_local
-from userbot import *
 
-
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36",
-    "content-type": "application/json",
-}
 
 def paste_text(text):
     asciich = ["**", "`", "__"]
@@ -147,44 +140,3 @@ def yaml_format(obj, indent=0, max_str_len=256, max_byte_len=64):
         return repr(obj)
 
     return "".join(result)
-
-def pasty(message, extension=None):
-    siteurl = "https://pasty.lus.pm/api/v1/pastes"
-    data = {"content": message}
-    try:
-        response = requests.post(url=siteurl, data=json.dumps(data), headers=headers)
-    except Exception as e:
-        return {"error": str(e)}
-    if response.ok:
-        response = response.json()
-        purl = (
-            f"https://pasty.lus.pm/{response['id']}.{extension}"
-            if extension
-            else f"https://pasty.lus.pm/{response['id']}.txt"
-        )
-        try:
-            await bot.send_message(
-                Config.MAFIABOT_LOGGER,
-                f"#PASTE \n\n**Open Paste From** [here]({purl}). \n**Delete that paste by using this token** `{response['deletionToken']}`",
-            )
-        except Exception as e:
-            LOGS.info(str(e))
-        return {
-            "url": purl,
-            "raw": f"https://pasty.lus.pm/{response['id']}/raw",
-            "bin": "Pasty",
-        }
-    return {"error": "Unable to reach pasty.lus.pm"}
-
-
-def telegraph_paste(page_title, temxt):
-    cl1ent = TelegraphPoster(use_api=True)
-    auth = "[ LEGENDARY AF MAFIABOT ]"
-    cl1ent.create_api_token(auth)
-    post_page = cl1ent.post(
-        title=page_title,
-        author=auth,
-        author_url="https://t.me/MafiaBot_Support",
-        text=temxt,
-    )
-    return post_page["url"]
