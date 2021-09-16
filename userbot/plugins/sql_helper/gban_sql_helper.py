ac607cd368c250d5e57bd49509785bd090b68ed6
H1M4N5HU0P <82947455+H1M4N5HU0P@users.noreply.github.com>
2021-09-16 14:00:46 +0530

    Update gban_sql_helper.py

--- userbot/plugins/sql_helper/gban_sql_helper.py
+++ userbot/plugins/sql_helper/gban_sql_helper.py
@@ -1,18 +1,3 @@
-"""
-credits to @mrconfused and @sandy1709
-thanks to catbot
-"""
-#    Copyright (C) 2020  sandeep.n(π.$)
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#   You should have received a copy of the GNU Affero General Public License
-#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 from sqlalchemy import Column, String
 
 from userbot.plugins.sql_helper import BASE, SESSION
@@ -34,26 +19,19 @@ GBan.__table__.create(checkfirst=True)
 def is_gbanned(chat_id):
     try:
         return SESSION.query(GBan).filter(GBan.chat_id == str(chat_id)).one()
-    except BaseException:
+    except:
         return None
     finally:
         SESSION.close()
 
 
-def get_gbanuser(chat_id):
-    try:
-        return SESSION.query(GBan).get(str(chat_id))
-    finally:
-        SESSION.close()
-
-
-def mafiagban(chat_id, reason):
+def catgban(chat_id, reason):
     adder = GBan(str(chat_id), str(reason))
     SESSION.add(adder)
     SESSION.commit()
 
 
-def mafiaungban(chat_id):
+def catungban(chat_id):
     rem = SESSION.query(GBan).get(str(chat_id))
     if rem:
         SESSION.delete(rem)
