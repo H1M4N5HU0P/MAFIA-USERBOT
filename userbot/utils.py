a1758909438f9b687f58c67f9bb8b47937b01ad9
H1M4N5HU0P <82947455+H1M4N5HU0P@users.noreply.github.com>
2021-09-20 20:12:11 +0530

    Add files via upload

--- userbot/utils.py
+++ userbot/utils.py
@@ -49,7 +49,7 @@ def load_module(shortname):
         spec = importlib.util.spec_from_file_location(name, path)
         mod = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(mod)
-        LOGS.info("Successfully imported " + shortname)
+        LOGS.info("MafiaBot - Successfully imported " + shortname)
     else:
         import userbot.utils
 
@@ -78,7 +78,7 @@ def load_module(shortname):
         spec.loader.exec_module(mod)
         # for imports
         sys.modules["userbot.plugins." + shortname] = mod
-        LOGS.info("Successfully imported " + shortname)
+        LOGS.info("⚡🔥MafiaBot⚡🔥 - Successfully imported " + shortname)
 
 
 def remove_plugin(shortname):
